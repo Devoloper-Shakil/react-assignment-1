dbd4d52191f6c0b6956a8afe189f52607cf87a49
shakil hasan <shshakilhasan69@gmail.com.com>
2021-03-03 12:31:30 -0800

    first commit

---
+++ src/components/Players/Players.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import bplData from '../../bplData.json';
+
+
+
+
+const Players = () => {
+    console .log(bplData);
+    // useEffect(() => {
+    //    console.log(bplData);
+    // }, [])
+
+    return (
+        
+        <div>
+            <h1>hello pelyers</h1>
+        </div>
+    );
+};
+
+export default Players;
